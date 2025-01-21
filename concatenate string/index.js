646d98babcc5639728d9a903ae7caadaf8b35587
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-21 14:50:26 +0600

    concatenate

---
+++ concatenate string/index.js
@@ -0,0 +1,8 @@
+//document.write("I" + "am" + "Nawsish");
+//firstName='Nawsish';
+//lastName=' Ahmed';
+//document.write("I am " +firstName+lastName)
+
+var num1=20;
+var num2=30;
+document.write("num1="+num1+ " and num2="+num2+ " and sum="+(num1+num2));
