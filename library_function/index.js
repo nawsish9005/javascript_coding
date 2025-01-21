ad578467edaa612f7f0fce1c3ed4e9274b273d49
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-21 15:24:08 +0600

    library func

---
+++ library_function/index.js
@@ -0,0 +1,20 @@
+var text1="Bangladesh";
+document.write(text1.length);
+
+var text2=prompt("Enter your name: ");
+
+document.write("Number of charecter " +text2.length);
+var text3="Bangladesh";
+
+document.write(text3.charAt(0));
+text4=text.toUpperCase();
+document.write(text4);
+
+var text5="Bangladesh ";
+var text6="is a beautiful country";
+
+document.write(text5.concat(text6));
+
+var text9="Bangladesh";
+text0=text9.slice(0, 2);
+document.write(text0);
