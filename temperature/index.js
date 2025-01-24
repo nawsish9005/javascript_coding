adc46527db8648776d6dd11624a8eeea103f1ab0
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-24 17:38:38 +0600

    update

--- temperature/index.js
+++ temperature/index.js
@@ -1,4 +1,10 @@
-var faren=prompt("Enter farenhite: ");
-var celc=(faren-32) * (5 / 9);
+var cels=prompt("Enter celsius: ");
+var farn=(cels * (9/5))+32;
 
-document.write("Celcius is"+ celc);
+document.write("farenhite is"+ farn);
+
+
+//var faren=prompt("Enter farenhite: ");
+//var celc=(faren-32) * (5 / 9);
+
+//document.write("Celcius is"+ celc);
