bc47c6c64a73bb5ea46c537f0201bd87a9b2840d
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-25 11:00:50 +0600

    update grade

--- if-else/index.js
+++ if-else/index.js
@@ -1,6 +1,23 @@
-var num =5;
+/*var num =5;
 
 if(num%2==0)
     document.write("Even"); 
 else
-    document.write("Odd");
+    document.write("Odd");*/
+
+var marks=prompt("Enter your marks: ");
+
+if(marks >= 80)
+    document.write("A+");
+else if(marks >= 70)
+    document.write("A");
+else if(marks >= 60)
+    document.write("A-");
+else if(marks >= 50)
+    document.write("B");
+else if(marks >= 40)
+    document.write("C");
+else if(marks >= 33)
+    document.write("D");
+else
+    document.write("Fail");
