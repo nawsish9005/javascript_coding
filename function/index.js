0bf0f430bb2d627901a58df7997ea47c2e4db270
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-29 13:10:37 +0600

    f

--- function/index.js
+++ function/index.js
@@ -23,3 +23,19 @@ function square2(num1, num2){
 
 var x= square2(10,10);
 document.write(" Result is: " + x);
+
+
+//IIFEs function
+//1 code
+
+(function square(){
+  var num=5;
+  var result2=num*num;
+  document.write("Result is " +result2);
+})()
+
+//2 code
+
+(function(a, b) {
+  document.write("Sum:", a + b);
+})(5, 3);
