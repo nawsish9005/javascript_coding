e3ecb8c7589e2dad6f81e2154390d08bc280c246
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-29 16:51:19 +0600

    array

---
+++ array/index.js
@@ -0,0 +1,17 @@
+var names = ["Nawsish","Iqbal","Riaj","Shoily"];
+
+document.write(names.length);
+
+names.push("Sumon");
+
+document.write(names.length);
+
+names.pop("Sumon");
+
+document.write(names.length);
+
+var car1=["Toyota","Suzuki"];
+var car2=["Ferrary","BMW"];
+
+var car= car1.concat(car2);
+document.write(car);
