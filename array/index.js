87b91d26007d6d15c210c448a49a787e9d478cfc
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-30 15:32:43 +0600

    ar

--- array/index.js
+++ array/index.js
@@ -15,3 +15,14 @@ var car2=["Ferrary","BMW"];
 
 var car= car1.concat(car2);
 document.write(car);
+
+//array loop
+var num=[10,20,30,40,50];
+var sum=0;
+
+for(var i=0; i<5; i++){
+    document.write(num[i]);
+    sum=sum+num[i];
+}
+
+document.write(sum);
