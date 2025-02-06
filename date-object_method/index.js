7120643b2627d4c46cb1ebc047674d2ad02dfabf
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-02-06 14:52:43 +0600

    upload math obj

---
+++ date-object_method/index.js
@@ -0,0 +1,11 @@
+var date=new Date();
+document.write(date);
+
+var year=new date.getFullYear();
+document.write(year);
+
+var month=new date.getMonth();
+document.write(month);
+
+var currentDate=date.getDate();
+document.write(currentDate);
