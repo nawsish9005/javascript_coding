49a2c0775ff60885c33e0a635f43ccffd19de538
Nawsish Ahmed <113119064+nawsish9005@users.noreply.github.com>
2025-02-06 11:49:35 +0600

    Update index.js

--- object/index.js
+++ object/index.js
@@ -1,17 +1,28 @@
+//constructor
+//adding function inside constructor
+
 function Student(name, age,cgpa, lang){
     this.name=name;
     this.age=age;
     this.cgpa=cgpa;
     this.lang=lang;
+
+    this.display=function(){
+        document.write(this.name);
+        document.write(this.age);
+        document.write(this.cgpa);
+        document.write(this.lang);
+    }
 }
 
-var student1= new Student();
+var student1= new Student("Nawsish Ahmed",27,3.94,["Bengali","Hindi","English"]);
 
-var student1={
-    name:"Nawsish Ahmed",
-    age:27,
-    cgpa:3.94,
-    lang:["Bengali","Hindi","English"]
-}
+//var student1={
+   // name:"Nawsish Ahmed",
+    //age:27,
+    //cgpa:3.94,
+    //lang:["Bengali","Hindi","English"]
+//}
 
-document.write(student1.age);
+//document.write(student1.name + " and " + student1.age);
+student1.display();
